7593e007b8c6d3182613c1dede17f18b492518b3
Peter Hartmann <peter@edelhirsch.io>
2021-01-20 15:31:38 +0100

    improve script

--- doc/generate-website.sh
+++ doc/generate-website.sh
@@ -8,13 +8,20 @@ cd "$(dirname "$0")"
 # generate XML if necessary:
 [ ! -d "xml" ] && doxygen
 
+# generate hugo site if necessary:
+[ ! -d "website" ] && hugo new site website
+[ ! -d "website/themes/hugo-book" ] && git clone https://github.com/alex-shpak/hugo-book.git website/themes/hugo-book
+
 # generate Markdown from XML:
 mkdir -p $OUTDIR
-doxybook2 -i xml -o $OUTDIR -c website/config.json
+doxybook2 -i xml -o $OUTDIR -c doxybook2-config.json
+rm -r website/content/Namespaces ### investigate namespaces with weird names
 
 # generate static HTML from Markdown:
+cp hugo-config.toml website/config.toml
 cd website
-[ ! -d "themes/hugo-book" ] && git clone https://github.com/alex-shpak/hugo-book.git themes/hugo-book
 hugo
 
-cp -r public $PUBDIR
+echo
+echo Done. Now do something like \"cp -r `realpath public` $PUBDIR\"
+echo
