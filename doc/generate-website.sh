4aa46ea002170739e78bff4ce20b7b23dc8bdefb
Peter Hartmann <peter@edelhirsch.io>
2021-02-19 10:09:19 +0100

    Revert "use templates"
    This reverts commit a28f98b43eb4568e9210bd15632438f8310261ca.

--- doc/generate-website.sh
+++ doc/generate-website.sh
@@ -63,7 +63,7 @@ then
 fi
 
 # generate Markdown from XML:
-$DOXYBOOK -i xml -o $DOXYBOOK_OUT -c doxybook2-config.json -t doxybook-templates
+$DOXYBOOK -i xml -o $DOXYBOOK_OUT -c doxybook2-config.json
 
 if [ $DOXYGEN_RUN = true ]
 then
