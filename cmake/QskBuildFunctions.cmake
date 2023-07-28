1b03d2190f9bcfa25c6aef99bfc816f757369db9
Peter Hartmann <peter@edelhirsch.io>
2023-07-28 19:32:09 +0200

    build system: Install examples and test support library
    ... so packages contain them as well.

--- cmake/QskBuildFunctions.cmake
+++ cmake/QskBuildFunctions.cmake
@@ -14,6 +14,8 @@ function(qsk_add_executable target)
     set_target_properties(${target} PROPERTIES
         RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin )
 
+    install(TARGETS ${target})
+
 endfunction()
 
 function(qsk_add_library target)
