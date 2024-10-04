5ed756e7d0dd3092acc06154c1156c9aab761cb5
Peter Hartmann <peter@edelhirsch.io>
2024-10-04 14:43:24 +0200

    text input: Also update metrics when updating node
    We need to recalculate alignment etc. when changing states

--- src/controls/QskTextInput.cpp
+++ src/controls/QskTextInput.cpp
@@ -517,6 +517,7 @@ void QskTextInput::updateLayout()
 void QskTextInput::updateNode( QSGNode* node )
 {
     m_data->textInput->updateColors();
+    m_data->textInput->updateMetrics();
     Inherited::updateNode( node );
 }
 
