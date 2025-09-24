efdcaa5b4a3737157173a6f1edfe284c1e3c7079
Peter Hartmann <peter@edelhirsch.io>
2025-09-24 17:46:44 +0200

    Revert "Add arc glow effect"
    This reverts commit f02809bc0a8cd732a2976e3af63833c8aced9501.
    
    glow and shadow are more or less the same apparently.

--- examples/tractorui/PowerLiftArcSkinlet.h
+++ examples/tractorui/PowerLiftArcSkinlet.h
@@ -19,7 +19,6 @@ class PowerLiftArcSkinlet : public QskSkinlet
             BoundariesRole,
             GrooveShadowRole,
             GrooveRole,
-            FillGlowRole,
             FillRole,
             ProgrammedFillRole,
             MinHandleRole,
@@ -41,7 +40,4 @@ class PowerLiftArcSkinlet : public QskSkinlet
 
     private:
         QSGNode* updateShadowNode( const PowerLiftArc* arc, QSGNode* node ) const;
-        QSGNode* updateGlowNode( const PowerLiftArc* arc, QSGNode* node ) const;
-
-        QskArcMetrics fillMetrics( const PowerLiftArc* arc ) const;
 };
